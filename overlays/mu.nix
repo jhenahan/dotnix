5e5730009fc6605c4d06f20031adb31385a49a70
Jack Henahan <root@proofte.ch>
2019-04-16 12:38:53 -0400

    Update mu

--- overlays/mu.nix
+++ overlays/mu.nix
@@ -1,17 +1,16 @@
 self:
   super:
 {
-  muHEAD = super.callPackage ../nixpkgs/pkgs/tools/networking/mu {
+  mu = (super.callPackage ../nixpkgs/pkgs/tools/networking/mu {
     texinfo = super.texinfo4;
     gmime = super.gmime3.overrideDerivation (drv: { doCheck = false; });
-  };
-  mu = self.muHEAD.overrideDerivation (drv: {
-    version = "9cf120b";
+  }).overrideDerivation (drv: {
+    version = "1.2";
     src = super.fetchFromGitHub {
       owner  = "djcb";
       repo   = "mu";
-      rev    = "9cf120b012cbe7ee4ef52299c1c0d66d08d32800";
-      sha256 = "1jz0sqfq2vz27yq3jjx3fmv9ygxis67pcac435a6a51cxi98i6ba";
+      rev    = "f62b4e534a7ef2acea3d87c54f7b4f19cec01afd";
+      sha256 = "0yhjlj0z23jw3cf2wfnl98y8q6gikvmhkb8vdm87bd7jw0bdnrfz";
     };
   });
 }
